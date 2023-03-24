349825e9e762a1a1584176d2b4836f726b3e05a3
Miguel Rico Feo <miguelrico2031@gmail.com>
2023-03-24 13:19:23 +0100

    golems cargan a otros jajajajajajaja

--- Assets/Scripts/Golems/Golem.cs
+++ Assets/Scripts/Golems/Golem.cs
@@ -12,15 +12,20 @@ public abstract class Golem : MonoBehaviour
         set { ChangeState(value); }
     }
 
-    [SerializeField] private GolemState _state;
+    [SerializeField] protected GameObject _topCollider;
 
+    private GolemState _state;
+    private LayerMask _groundGolemLayer;
     protected Rigidbody2D _rb;
     protected Collider2D _collider;
+    
 
     protected virtual void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
-        _collider = GetComponent<Collider2D>();      
+        _collider = GetComponent<Collider2D>();
+
+        _groundGolemLayer = LayerMask.GetMask(new string[] { "GroundGolem" });
     }
 
     protected virtual void Start()
@@ -36,17 +41,26 @@ public abstract class Golem : MonoBehaviour
             case GolemState.Disabled:
                 _rb.isKinematic = true;
                 _collider.enabled = false;
+                if (_topCollider) _topCollider.SetActive(false);
                 break;
 
             case GolemState.Enabled:
                 _rb.isKinematic = false;
                 _collider.enabled = true;
+                if (_topCollider) _topCollider.SetActive(true);
+                if (transform.parent != null) transform.parent = null;
                 break;
 
             case GolemState.Available:
                 //_rb.isKinematic = true;  voy a dejar que cada golem active su isKinematic a su tiempo
                 _collider.enabled = true;
+                
+                Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.65f, _groundGolemLayer);
+                if (colliders.Length > 0) transform.SetParent(colliders[0].transform);
+
+                if (_topCollider) _topCollider.SetActive(true);
                 break;
+
             case GolemState.BeingLaunched:
                 _rb.isKinematic = false;
                 _collider.enabled = true;
