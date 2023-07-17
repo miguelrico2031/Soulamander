5683eace9b1b3ebdc7f7df6f764c9b184444a3e5
Borjita <borja.fierro@gmail.com>
2023-07-17 16:05:07 +0200

    Pushable Actualizados

--- Assets/Scripts/Interactables/PushableObject.cs
+++ Assets/Scripts/Interactables/PushableObject.cs
@@ -10,10 +10,9 @@ public class PushableObject : MonoBehaviour
     [SerializeField] private LayerMask _interactableLayers;
     [SerializeField] private LayerMask _groundLayer;
     [SerializeField] private LayerMask _golemLayer;
-    //[SerializeField] private float _gravityForce;
     [SerializeField] private Rigidbody2D _rb;
     [SerializeField] private BoxCollider2D _collider;
-    [SerializeField] private float _groundCheckOffset, _wallCheckOffset;
+    [SerializeField] private float _wallCheckOffset;
 
     private bool _isGrounded;
     private Collider2D[] _wallHitColliders;
@@ -31,58 +30,10 @@ public class PushableObject : MonoBehaviour
 
     private void FixedUpdate()
     {
-        WallAndRammerCheck();
-
-        _rb.isKinematic = HasHitWall && RayCastHitGround();
+        WallCheck();
     }
-
-    private bool RayCastHitGround()
-    {
-        RaycastHit2D[] raycastHit = new RaycastHit2D[3];
-
-        raycastHit[0] = Physics2D.Raycast(_collider.bounds.center, Vector2.down, _collider.bounds.extents.y + _groundCheckOffset, _groundLayer);
-
-        raycastHit[1] = Physics2D.Raycast(new Vector2(_collider.bounds.center.x + _collider.bounds.extents.x, _collider.bounds.center.y), Vector2.down, _collider.bounds.extents.y + _groundCheckOffset, _groundLayer);
-
-        raycastHit[2] = Physics2D.Raycast(new Vector2(_collider.bounds.center.x - _collider.bounds.extents.x, _collider.bounds.center.y), Vector2.down, _collider.bounds.extents.y + _groundCheckOffset, _groundLayer);
-
-        foreach (var hit in raycastHit)
-        {
-            if (hit.collider != null)
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-    /*
-    private GameObject RayCastHitWall(LayerMask layer)
-    {
-        RaycastHit2D[] raycastHit = new RaycastHit2D[3];
-
-        raycastHit[0] = Physics2D.Raycast(new Vector2(_collider.bounds.center.x - _collider.bounds.extents.x - _groundCheckOffset, _collider.bounds.center.y), Vector2.right, 2 * _collider.bounds.extents.x + 2 * _groundCheckOffset, layer);
-
-        raycastHit[1] = Physics2D.Raycast(new Vector2(_collider.bounds.center.x - _collider.bounds.extents.x - _groundCheckOffset, _collider.bounds.center.y + _collider.bounds.extents.y), Vector2.right, 2 * _collider.bounds.extents.x + 2 * _groundCheckOffset, layer);
-
-        raycastHit[2] = Physics2D.Raycast(new Vector2(_collider.bounds.center.x - _collider.bounds.extents.x - _groundCheckOffset, _collider.bounds.center.y - _collider.bounds.extents.y), Vector2.right, 2 * _collider.bounds.extents.x + 2 * _groundCheckOffset, layer);
-
-        foreach (var hit in raycastHit)
-        {
-            if (hit.collider != null)
-            {
-                return hit.collider.gameObject;
-            }
-        }
-        return null;
-    }
-    */
-    //private void OnDrawGizmos()
-    //{
-    //    Gizmos.DrawCube(_collider.bounds.center, _collider.size * 0.9f + Vector2.right * _collider.size.x * _wallCheckOffset);
-    //}
-
-
-    private void WallAndRammerCheck()
+ 
+    private void WallCheck()
     {
         HasHitWall = false;
 
